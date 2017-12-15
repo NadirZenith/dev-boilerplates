decc67f5ef10ab2c4d5b698be09f654726a974f0
NadirZenith <2cb.md2@gmail.com>
2017-12-15 14:30:23 +0100

    added authentication with passport and session with mongoose

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ import webpack from 'webpack';
 import mongoose from 'mongoose';
 import Express from 'express';
 import session from 'express-session';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import flash from 'connect-flash';
 
 import morgan from 'morgan'; // express midleware log
 import bodyParser from 'body-parser'; // express midleware request parser
@@ -16,6 +19,19 @@ import mainRoutes from './routes/main.routes';
 
 // Express --------------------------------------------
 const app = Express();
+
+// log
+app.use(morgan('dev'));
+// app.use(morgan('tiny'));
+// app.use(morgan('combined'))
+
+app.use(cookieParser());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
 
 // ------ dev
 // Webpack Requirements
@@ -47,30 +63,146 @@ mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 
-// ---------- midlewares ------------------------------
-// session
-const MongoStore = require('connect-mongo')(session);
+// session --------------------------------------------
 
-const sess = {
+// const MongoStore = require('connect-mongo')(session);
+// const sess = {
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   cookie: {},
+//   store: new MongoStore({ mongooseConnection: mongoose.connection }),
+// };
+// app.use(session(sess));
+
+const sessFile = {
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
-  cookie: {},
-  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+};
+app.use(session(sessFile));
+
+app.use(flash());
+
+// security -------------------------------------------
+const LocalStrategy = require('passport-local').Strategy;
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy((username, password, done) => {
+  try {
+    if (username === 'tino') {
+      if (password === 'tino') {
+        return done(null, { id: 1, secret: 'foo' });
+      }
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, false, { message: 'User not found.' });
+  } catch (err) {
+    console.log(err)
+    return done('unknown error');
+  }
+
+  // User.findOne({ username: username }, (err, user) => {
+  //   if (err) { return done(err); }
+  //   if (!user) {
+  //     return done(null, false, { message: 'Incorrect username.' });
+  //   }
+  //   if (!user.validPassword(password)) {
+  //     return done(null, false, { message: 'Incorrect password.' });
+  //   }
+  //   return done(null, user);
+  // });
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  if (id === 1) {
+    return done(null, { id: 1, name: 'tininho' });
+  }
+  return done('no match serializing');
+  // User.findById(id, (err, user) => {
+  //   done(err, user);
+  // });
+});
+
+const checkAuthentication = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
 };
 
-app.use(session(sess));
+
+app.post('/login', passport.authenticate('local', {
+  successRedirect: '/me',
+  failureRedirect: '/login',
+  failureFlash: true,
+}));
+
+app.get('/login', (req, res) => {
+  const message = req.flash('error');
+
+  res.send(`
+    <form action="/login" method="post">
+    ${message}
+    <div>
+        <label>Username:</label>
+        <input type="text" name="username"/>
+    </div>
+    <div>
+        <label>Password:</label>
+        <input type="password" name="password"/>
+    </div>
+    <div>
+        <input type="submit" value="Log In"/>
+    </div>
+  </form>`);
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+app.get('/', (req, res) => {
+  const menu = req.isAuthenticated()
+    ? '<a href="/me">Me</a>'
+    : '<a href="/login">Login</a>';
+
+  res.send(`
+  ${menu}<br>
+  Hello World!
+  `);
+});
+
+app.get('/me', checkAuthentication, (req, res) => {
+  const menu = '<a href="/logout">Logout</a>';
+
+  res.send(`
+  ${menu}<br>
+  Hello ${req.user.name}!
+  `);
+});
 
 
-// log
-app.use(morgan('tiny'));
-// app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.get(
+//   '/api/me', passport.authenticate('local', { session: true }),
+//   (req, res) => {
+//     res.json(req.user);
+//   },
+// );
+
 
 // ---------- routes ------------------------------
 // app.use(Express.static('public'))
 // app.use(Express.static(path.resolve(__dirname, '../client')));
-app.use(mainRoutes);
+// app.use(mainRoutes);
 
 // Express start ----------------------------------
 app.listen(serverConfig.port, () => {
