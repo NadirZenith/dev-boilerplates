3550c9cffa298f9998308277789c0a952db6eac9
NadirZenith <2cb.md2@gmail.com>
2017-12-15 10:33:39 +0100

    added session with mongo

--- server/server.js
+++ server/server.js
@@ -1,49 +1,80 @@
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpack from 'webpack';
 
+import mongoose from 'mongoose';
+import Express from 'express';
+import session from 'express-session';
+
+import morgan from 'morgan'; // express midleware log
+import bodyParser from 'body-parser'; // express midleware request parser
+
+import config from '../config/webpack.config.dev';
 import serverConfig from './config';
-import Express from 'express'
+import mainRoutes from './routes/main.routes';
+
 
 // Express --------------------------------------------
-const app = Express()
+const app = Express();
 
 // ------ dev
 // Webpack Requirements
-import webpack from 'webpack';
-import config from '../config/webpack.config.dev';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-
 // Run Webpack dev server in development mode
 if (process.env.NODE_ENV === 'development') {
   const compiler = webpack(config);
-  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  }));
   app.use(webpackHotMiddleware(compiler));
 
   // cors
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
     next();
   });
-}else{
-  app.use(Express.static('dist/client'))
+} else {
+  app.use(Express.static('dist/client'));
 }
 
+
+// db --------------------------------------------
+mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+mongoose.Promise = global.Promise;
+
+
 // ---------- midlewares ------------------------------
-import morgan from 'morgan' // express midleware log
-import bodyParser from 'body-parser' // express midleware request parser
-app.use(morgan('tiny'))
+// session
+const MongoStore = require('connect-mongo')(session);
+
+const sess = {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {},
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+};
+
+app.use(session(sess));
+
+
+// log
+app.use(morgan('tiny'));
 // app.use(morgan('combined'))
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // ---------- routes ------------------------------
-import mainRoutes from './routes/main.routes';
 // app.use(Express.static('public'))
 // app.use(Express.static(path.resolve(__dirname, '../client')));
 app.use(mainRoutes);
 
 // Express start ----------------------------------
-app.listen(serverConfig.port, () => console.log(`Example app listening on port ${serverConfig.port}!`))
+app.listen(serverConfig.port, () => {
+  console.log(`Example app listening on port ${serverConfig.port}!`);
+});
 
 export default app;
